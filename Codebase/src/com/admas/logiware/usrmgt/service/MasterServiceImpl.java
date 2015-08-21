5f891dd0ab13587ebf5359401c7b47b4bafb445b
Admas Technologies <itamol.shelke@gmail.com>
2015-08-22 01:50:07 +0530

    Raj

--- Codebase/src/com/admas/logiware/usrmgt/service/MasterServiceImpl.java
+++ Codebase/src/com/admas/logiware/usrmgt/service/MasterServiceImpl.java
@@ -31,7 +31,7 @@ public class MasterServiceImpl{
 
 	/** The logger. */
 	Logger logger = LoggerFactory.getLogger(MasterServiceImpl.class);
-	
+
 	@Autowired
 	@Qualifier("serviceInvoker")
 	private ServiceInvoker serviceInvoker;
@@ -69,7 +69,7 @@ public class MasterServiceImpl{
 			HashMap<String, Object> reqDtoObjects,
 			Map<String, Object> resDtoObjects) {
 
-		
+
 		logger.info("MasterServiceImpl getAllCity method start. ");
 		City city=new City();
 		City city1=new City();
@@ -80,7 +80,7 @@ public class MasterServiceImpl{
 		String viewName="";
 		try {
 			viewName="getAllCities";
-//			city = doServiceCall(flowData, ServiceName.getAllCity, reqDtoObjects);
+			//			city = doServiceCall(flowData, ServiceName.getAllCity, reqDtoObjects);
 			state.setId(1);
 			state.setStateName("MH");
 			state1.setId(2);
@@ -96,7 +96,7 @@ public class MasterServiceImpl{
 			resDtoObjects.put("lCity", lCities);
 			resDtoObjects.put("lState", lStates);
 			resDtoObjects.put(WebAppConstants.VIEW_NAME, viewName);
-		/*} catch (LogiwareBaseException b) {
+			/*} catch (LogiwareBaseException b) {
 			throw b;*/
 		} catch (Exception e) {
 			logger.error("Exception In MasterServiceImpl getAllCity client method end.",
@@ -109,13 +109,13 @@ public class MasterServiceImpl{
 		logger.info("MasterServiceImpl getAllCity() method end. ");
 		//return responseUser;
 		return resDtoObjects;
-		
+
 	}
 
 	public Map<String, Object> showEditCity(FlowData flowData,
 			HashMap<String, Object> reqDtoObjects,
 			Map<String, Object> resDtoObjects) {
-		
+
 		logger.info("MasterServiceImpl editCity() method Start. ");
 		String viewName = "";
 		try {
@@ -125,17 +125,17 @@ public class MasterServiceImpl{
 			city.setId(1);
 			resDtoObjects.put(WebAppConstants.VIEW_NAME, viewName);
 			resDtoObjects.put("city", city)
-;		} catch (Exception exp) {
-			logger.error("Exception in editCity()", exp);
-		}
+			;		} catch (Exception exp) {
+				logger.error("Exception in editCity()", exp);
+			}
 		logger.info("MasterServiceImpl editCity() method end. ");
 		return resDtoObjects;
 	}
-	
+
 	public Map<String, Object> saveCity(FlowData flowData,
 			HashMap<String, Object> reqDtoObjects,
 			Map<String, Object> resDtoObjects) {
-		
+
 		logger.info("MasterServiceImpl getAllCity method start. ");
 		City city=new City();
 		City city1=new City();
@@ -143,14 +143,14 @@ public class MasterServiceImpl{
 		String viewName="";
 		try {
 			viewName="getAllCities";
-//			city = doServiceCall(flowData, ServiceName.getAllCity, reqDtoObjects);
+			//			city = doServiceCall(flowData, ServiceName.getAllCity, reqDtoObjects);
 			city.setCityName("Pune");
 			city1.setCityName("Mumbai");
 			lCities.add(city);
 			lCities.add(city1);
 			resDtoObjects.put("lCity", lCities);
 			resDtoObjects.put(WebAppConstants.VIEW_NAME, viewName);
-		/*} catch (LogiwareBaseException b) {
+			/*} catch (LogiwareBaseException b) {
 			throw b;*/
 		} catch (Exception e) {
 			logger.error("Exception In MasterServiceImpl getAllCity client method end.",
@@ -164,11 +164,140 @@ public class MasterServiceImpl{
 		//return responseUser;
 		return resDtoObjects;
 	}
+
+
+
+	public Map<String, Object> getAllTransportTypes(FlowData flowData,
+			HashMap<String, Object> reqDtoObjects,
+			Map<String, Object> resDtoObjects) {
+
+		logger.info("MasterServiceImpl getAllTransportTypes() method start. ");
+		TransportType type=new TransportType();
+		TransportType type1=new TransportType();
+		List<TransportType> lTransports=new ArrayList<TransportType>();
+		String viewName="";
+		try {
+			viewName="getAllTransportTypes";
+			//			city = doServiceCall(flowData, ServiceName.getAllCity, reqDtoObjects);
+			type.setId(1);
+			type.setName("ADMAS");
+			type.setDescription("ADMASTECH...");
+			type1.setId(2);
+			type1.setName("HENX");
+			type1.setDescription("henx...");
+			lTransports.add(type);
+			lTransports.add(type1);
+
+			resDtoObjects.put("lTransports", lTransports);
+			resDtoObjects.put(WebAppConstants.VIEW_NAME, viewName);
+			/*} catch (LogiwareBaseException b) {
+			throw b;*/
+		} catch (Exception e) {
+			logger.error("Exception In MasterServiceImpl  getAllTransportTypes() method end.",
+					e);
+			/*throw new LogiwareBaseException(
+					LogiwarePortalErrors.INVALID_REQUEST.getErrorCode(),
+					LogiwarePortalErrors.INVALID_REQUEST
+							.getErrorDescription());*/
+		}
+		logger.info("MasterServiceImpl  getAllTransportTypes() method end. ");
+		//return responseUser;
+		return resDtoObjects;
+
+
+
+	}
+
+	public Map<String, Object> showAddTransportType(FlowData flowData,
+			HashMap<String, Object> reqDtoObjects,
+			Map<String, Object> resDtoObjects) {
+
+
+
+		String viewName = "";
+		try {
+			viewName = "showAddTranceportType";
+			TransportType transportType=new TransportType();
+			resDtoObjects.put("transportType", transportType);
+
+		} catch (Exception exp) {
+			logger.error("Exception in TransportType()", exp);
+		}
+
+		resDtoObjects.put(WebAppConstants.VIEW_NAME, viewName);
+		return resDtoObjects;
+	}
+
+	public Map<String, Object> saveTransportType(FlowData flowData,
+			HashMap<String, Object> reqDtoObjects,
+			Map<String, Object> resDtoObjects) {
+
+
+		logger.info("MasterServiceImpl saveTransportType method start. ");
+		TransportType type=new TransportType();
+		TransportType type1=new TransportType();
+		List<TransportType> lTransports=new ArrayList<TransportType>();
+		String viewName="";
+		try {
+			viewName="getAllTransportTypes";
+			//			city = doServiceCall(flowData, ServiceName.getAllCity, reqDtoObjects);
+			type.setId(1);
+			type.setName("ADMAS");
+			type.setDescription("ADMASTECH...");
+			type1.setId(2);
+			type1.setName("HENX");
+			type1.setDescription("henx...");
+			lTransports.add(type);
+			lTransports.add(type1);
+
+			resDtoObjects.put("lTransports", lTransports);
+			resDtoObjects.put(WebAppConstants.VIEW_NAME, viewName);
+			/*} catch (LogiwareBaseException b) {
+			throw b;*/
+		} catch (Exception e) {
+			e.printStackTrace();
+			logger.error("Exception In MasterServiceImpl saveTransportType  method end.", e);
+
+			/*throw new LogiwareBaseException(
+					LogiwarePortalErrors.INVALID_REQUEST.getErrorCode(),
+					LogiwarePortalErrors.INVALID_REQUEST
+							.getErrorDescription());*/
+		}
+		logger.info("MasterServiceImpl saveTransportType() method end. ");
+		//return responseUser;
+		return resDtoObjects;
+
+	}
+
+	public Map<String, Object> showEditTransportType(FlowData flowData,
+			HashMap<String, Object> reqDtoObjects,
+			Map<String, Object> resDtoObjects) {
+
+		logger.info("MasterServiceImpl showEditTransportType() method Start. ");
+		String viewName = "";
+		try {
+			viewName = "showAddTranceportType";
+			TransportType type=new TransportType();
+			type.setCompanyId(123);
+			type.setDescription("XXXXXX");
+			logger.info("########################"+reqDtoObjects.get("id"));
+			type.setId(Integer.parseInt(reqDtoObjects.get("id").toString()));
+			type.setName("ASDASD");
+
+			resDtoObjects.put(WebAppConstants.VIEW_NAME, viewName);
+			resDtoObjects.put("transportType",type);
+		} catch (Exception exp) {
+			logger.error("Exception in showEditTransportType()", exp);
+		}
+		logger.info("MasterServiceImpl showEditTransportType() method End. ");
+		return resDtoObjects;
+	}
+
 	public Map<String, Object> showDeleteTransportType(FlowData flowData,
 			HashMap<String, Object> reqDtoObjects,
 			Map<String, Object> resDtoObjects) {
-		
-		
+
+
 		logger.info("MasterServiceImpl showDeleteTransportType() method Start. ");
 		TransportType type=new TransportType();
 		TransportType type1=new TransportType();
@@ -176,7 +305,7 @@ public class MasterServiceImpl{
 		String viewName="";
 		try {
 			viewName="getAllTransportTypes";
-//			city = doServiceCall(flowData, ServiceName.getAllCity, reqDtoObjects);
+			//			city = doServiceCall(flowData, ServiceName.getAllCity, reqDtoObjects);
 			System.out.println("#########delete###############"+reqDtoObjects.get("id"));
 			type.setId(1);
 			type.setName("ADMAS");
@@ -186,11 +315,11 @@ public class MasterServiceImpl{
 			type1.setDescription("henx...");
 			lTransports.add(type);
 			lTransports.add(type1);
-			
+
 			resDtoObjects.put("lTransports", lTransports);
 			resDtoObjects.put(WebAppConstants.VIEW_NAME, viewName);
-			
-			} catch (Exception exp) {
+
+		} catch (Exception exp) {
 			logger.error("Exception in showDeleteTransportType()", exp);
 		}
 		logger.info("MasterServiceImpl showDeleteTransportType() method End. ");
@@ -200,8 +329,8 @@ public class MasterServiceImpl{
 	public Map<String, Object> showDeleteCity(FlowData flowData,
 			HashMap<String, Object> reqDtoObjects,
 			Map<String, Object> resDtoObjects) {
-		
-		
+
+
 		logger.info("MasterServiceImpl showDeleteCity method start. ");
 		City city=new City();
 		City city1=new City();
@@ -212,7 +341,7 @@ public class MasterServiceImpl{
 		String viewName="";
 		try {
 			viewName="getAllCities";
-//			city = doServiceCall(flowData, ServiceName.getAllCity, reqDtoObjects);
+			//			city = doServiceCall(flowData, ServiceName.getAllCity, reqDtoObjects);
 			state.setId(1);
 			state.setStateName("MH");
 			state1.setId(2);
@@ -228,7 +357,7 @@ public class MasterServiceImpl{
 			resDtoObjects.put("lCity", lCities);
 			resDtoObjects.put("lState", lStates);
 			resDtoObjects.put(WebAppConstants.VIEW_NAME, viewName);
-		/*} catch (LogiwareBaseException b) {
+			/*} catch (LogiwareBaseException b) {
 			throw b;*/
 		} catch (Exception e) {
 			logger.error("Exception In MasterServiceImpl showDeleteCity client method end.",
@@ -241,32 +370,31 @@ public class MasterServiceImpl{
 		logger.info("MasterServiceImpl showDeleteCity() method end. ");
 		//return responseUser;
 		return resDtoObjects;
-		
+
 	}
 
+
 	public ModelAndView getAllBranches(FlowData flowData, HashMap<String, Object> resDtoObject) {
-			BranchDetail detail=new BranchDetail();
-			detail.setBranchName("Hyderabad Branch");
-			List<BranchDetail> branchList = new ArrayList<BranchDetail>();  
-			branchList.add(detail);
-			resDtoObject.put("allBranch", branchList);
-			String viewName= "";
-			try{
+		BranchDetail detail=new BranchDetail();
+		detail.setBranchName("Hyderabad Branch");
+		List<BranchDetail> branchList = new ArrayList<BranchDetail>();  
+		branchList.add(detail);
+		resDtoObject.put("allBranch", branchList);
+		String viewName= "";
+		try{
 			viewName="branchTiles";	
 			resDtoObject.put(WebAppConstants.VIEW_NAME, viewName);	
 			return new ModelAndView((String) resDtoObject.get(WebAppConstants.VIEW_NAME),resDtoObject);
-			}
-			catch(Exception e){
-				
-			}
-			return null;
+		}
+		catch(Exception e){
+
+		}
+		return null;
 	}
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
 }
 
